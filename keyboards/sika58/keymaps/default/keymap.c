7e1c55aee3ffba8eed1d1322b5aa8d6e420394bc
fivedee <64674962+fivedee@users.noreply.github.com>
2023-01-11 16:22:14 -0800

    Started on sika58.h and the default keymap

--- keyboards/sika58/keymaps/default/keymap.c
+++ keyboards/sika58/keymaps/default/keymap.c
@@ -3,6 +3,11 @@
 
 #include QMK_KEYBOARD_H
 
+enum layers {
+	_BASE = 0,
+	_FN,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
@@ -15,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ 0 │ . │Ent│ + │
      * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x4(
+    [0] = LAYOUT_split_6x4(
         KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
         KC_P4,   KC_P5,   KC_P6,   KC_PAST,
         KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
--- keyboards/sika58/sika58.c
+++ keyboards/sika58/sika58.c
@@ -1,3 +1,4 @@
 // Copyright 2023 Noelle (@fivedee)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "sika58.h"
